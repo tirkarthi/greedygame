0ac8d145253275dedb280a2d99f5fc10afa6944c
karthikeyan <tir.karthi@gmail.com>
2016-07-25 13:47:21 +0530

    Fix migrations and start from scratch

--- music_collection/migrations/0001_initial.py
+++ music_collection/migrations/0001_initial.py
@@ -13,17 +13,19 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Genre',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
-                ('Title', models.CharField(max_length=200)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=200)),
+                ('modified', models.DateTimeField(auto_now=True)),
             ],
         ),
         migrations.CreateModel(
             name='Track',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
-                ('Title', models.CharField(max_length=200)),
-                ('Rating', models.IntegerField(default=4)),
-                ('Genre', models.ForeignKey(to='music_collection.Genre')),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=200)),
+                ('rating', models.IntegerField(default=4)),
+                ('modified', models.DateTimeField(auto_now=True)),
+                ('genre', models.ManyToManyField(to='music_collection.Genre')),
             ],
         ),
     ]
