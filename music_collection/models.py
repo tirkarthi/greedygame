46919c9a52531692353cf0361071241103e8e635
karthikeyan <tir.karthi@gmail.com>
2016-07-27 11:39:08 +0530

    Allow null for modified for migrations

--- music_collection/models.py
+++ music_collection/models.py
@@ -5,11 +5,11 @@ from django.db import models
 
 class Genre(models.Model):
     title = models.CharField(max_length=200)
-    modified = models.DateTimeField(auto_now=True, blank=True)
+    modified = models.DateTimeField(auto_now=True, blank=True, null=True)
 
 
 class Track(models.Model):
     title = models.CharField(max_length=200)
     rating = models.IntegerField(default=4)
     genre = models.ManyToManyField(Genre)
-    modified = models.DateTimeField(auto_now=True, blank=True)
+    modified = models.DateTimeField(auto_now=True, blank=True, null=True)
