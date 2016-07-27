9f1d8c004ca8ae71adb9c29364f52ac742cb0bb1
karthikeyan <tir.karthi@gmail.com>
2016-07-27 11:23:04 +0530

    Fix migrations for tests

--- music_collection/migrations/0001_initial.py
+++ music_collection/migrations/0001_initial.py
@@ -13,18 +13,18 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Genre',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
                 ('title', models.CharField(max_length=200)),
-                ('modified', models.DateTimeField(auto_now=True)),
+                ('modified', models.DateTimeField(auto_now=True, null=True)),
             ],
         ),
         migrations.CreateModel(
             name='Track',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
                 ('title', models.CharField(max_length=200)),
                 ('rating', models.IntegerField(default=4)),
-                ('modified', models.DateTimeField(auto_now=True)),
+                ('modified', models.DateTimeField(auto_now=True, null=True)),
                 ('genre', models.ManyToManyField(to='music_collection.Genre')),
             ],
         ),
