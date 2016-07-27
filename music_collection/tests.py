960145adecad8a73bc530a09acafe9f42f3b11f9
karthikeyan <tir.karthi@gmail.com>
2016-07-27 11:38:30 +0530

    Initial test

--- music_collection/tests.py
+++ music_collection/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from rest_framework.test import APIClient
+from .models import *
+
+class MyTestCase(TestCase):
+
+    fixtures = ['fixtures.json']
+
+    def test_get_track(self):
+        api_client = APIClient()
+        req = api_client.get('/api/tracks/38')
+        self.assertEqual(req.status_code, 200)
+        self.assertEqual(req.data['rating'], 4)
